5db9fb2b856cd7a6aa3b6d1830fb943e0ad689ac
Devrath <devrath.rana@kelltontech.com>
2022-06-14 09:04:17 +0530

    fix

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,28 @@
+import _ from 'loadash';//By convention is represented by underscore
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {connect} from 'react-redux';
+import {ListView, View, Text} from 'react-native';
+import {employeesFetch} from '../actions';
 
 class EmployeeList extends Component {
+  componentWillUnmount() {
+    this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //nextProps are the next set of props that this component will be rendered with this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  //This will recreate our datasource each ttime we fetch data from firebase
+  createDataSource({employees}) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+    this.dataSource = ds.cloneWithRows(employees);//It will accpt array of objects so in mapStateToProps method we also need to  convert it into array of objects using loadash
+  }
+
   render() {
     return (
       <View>
@@ -10,4 +31,6 @@ class EmployeeList extends Component {
     );
   }
 }
-export default EmployeeList;
+
+const mapStateToProps = state => {};
+export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,20 @@
+import {EMPLOYEE_UPDATE,EMPLOYEE_CREATE} from '../actions/types';
+
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: '',
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
+      case EMPLOYEE_UPDATE:
+      // newSTate = {}
+      // newState[action.payload.prop] = action.payload.value iska alternative key interpolation
+      return {...state, [action.payload.prop]: action.payload.value}; //here squre bracket is not array but key interpolation i.e. we are passing the key of this object at time of creation of this object here
+    default:
+      return state;
+  }
+};
